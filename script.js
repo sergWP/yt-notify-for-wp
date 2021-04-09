99a22c46093c8f423dfb0f3c7549e92c459a23c6
sergWP <56840586+sergWP@users.noreply.github.com>
2021-04-09 16:58:14 +0300

    удаление левого кода

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 jQuery(document).ready(function($) {
-    $('.style-2 .cactus-post-item .cactus-post-title a').each(function() {
-        if($(this).text().length > 70) {
-            $(this).text($(this).text().substring(0,70)+"...");
-        }
-    });
-    $('.block-articles .cactus-post-title a').each(function() {
-        if($(this).text().length > 60) {
-            $(this).text($(this).text().substring(0,60)+"...");
-        }
-    });
-
-
-    $('.prev-next-slider').remove();
-
-    $('.tab-label').on('click', function () {
-        let currentTab = $(this).attr('data-tab');
-        $('.tab-label').removeClass('active-label');
-        $(this).addClass('active-label');
-        $('.cactus-tabs .block-wrap').hide();
-        $('.cactus-tabs').find('.block-'+currentTab).show();
-    });
-
-    $('.open-menu').on('click', function () {
-        $('.modal-menu, .menu-overlay').addClass('active');
-        $('body').addClass('no-scroll');
-    });
-
-    $('.close-menu').on('click', function () {
-        $('.modal-menu, .menu-overlay').removeClass('active');
-        $('body').removeClass('no-scroll');
-    });
-
-
     // clean bell
     let myajax = `//${window.location.host}/wp-admin/admin-ajax.php`,
         paramObj = {
